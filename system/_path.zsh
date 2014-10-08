bdd9d2292facd9b9eef5c5337f23f1ec8b6b4a35
Laszlo Tamas <tamas.lacko@gmail.com>
2014-10-08 18:16:56 +0200

    Extend path with zend and composer bins

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+zend=/usr/local/zend/mysql/bin:/usr/local/zend/bin
+composer=$HOME/.composer/vendor/bin
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$zend:$composer:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
